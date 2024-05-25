1973212356f423ffb8bd2cfe3356d19e83f07ed7
PenJonathan <154846198+PenJonathan@users.noreply.github.com>
2024-05-25 18:22:21 -0400

    Create Turret.ino

---
+++ Turret/Turret.ino
@@ -0,0 +1,118 @@
+#include "IRremote.h"
+#include <Servo.h>
+#include <Stepper.h>
+
+
+#define receiver A0
+//int receiver = 11;
+
+Stepper horizontal(2048, 6, 8, 7, 9);
+Stepper vertical_1(2048, 10, 12, 11, 13);
+Stepper vertical_2(2048, )
+int motor_control = 5;
+int d1 = 12;
+int d2 = 13;
+int motor_state = 0;
+
+Servo shoot_ammo;
+
+//////////////////////      Receiver Stuff
+IRrecv irrecv(receiver);
+uint32_t last_decodedRawData = 0;
+void translateIR()
+{
+  if (irrecv.decodedIRData.flags)
+  {
+    irrecv.decodedIRData.decodedRawData = last_decodedRawData;
+  } 
+  else
+  {
+    Serial.print("IR code:0x");
+    Serial.println(irrecv.decodedIRData.decodedRawData, HEX);
+  }
+  switch (irrecv.decodedIRData.decodedRawData)
+  {
+    case 0xBA45FF00: Serial.println("motor"); motor(); break;
+    case 0xBF40FF00: Serial.println("shoot"); shoot();    break;
+    case 0xBB44FF00: Serial.println("left"); left();   break;
+    case 0xBC43FF00: Serial.println("right");  right(); break;
+    case 0xB946FF00: Serial.println("up");    up(); break;
+    case 0xEA15FF00: Serial.println("down");   down(); break;
+    default:
+      Serial.println(" other button   ");
+  }
+  last_decodedRawData = irrecv.decodedIRData.decodedRawData;
+  delay(100);
+}
+//////////////////////      Stepper Motor Stuff
+void left(){
+  horizontal.step(-100);
+}
+
+void right(){
+ horizontal.step(100);
+}
+
+void up(){
+  vertical_1.step(100);
+
+}
+
+void down(){
+  vertical_1.step(-100);
+}
+//////////////////////      Servo Connect Motor Stuff
+void motor(){
+  if(motor_state == 0){
+    analogWrite(motor_control, 255);
+    digitalWrite(d1, HIGH);
+    digitalWrite(d2, LOW);
+    Serial.print(" on");
+    motor_state = 1;
+  }
+
+  else if(motor_state == 1){
+    analogWrite(motor_control, 0);
+    digitalWrite(d1, HIGH);
+    digitalWrite(d2, LOW);
+    Serial.print(" off");
+    motor_state = 0;
+  }
+}
+
+//////////////////////      Ammo Loader Stuff
+void shoot(){
+  shoot_ammo.write(90);
+  delay(500);
+  shoot_ammo.write(0);
+}
+
+
+
+//////////////////////      Setup Stuff
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  horizontal.setSpeed(10);
+  vertical_1.setSpeed(10);
+  vertical_2.setSpeed(10);
+  shoot_ammo.attach(3);
+  pinMode(motor_control, OUTPUT);
+  pinMode(d1, OUTPUT);
+  pinMode(d2, OUTPUT);
+  pinMode(receiver, INPUT);
+}
+
+
+//////////////////////      Running Stuff
+void loop()
+{
+  if (irrecv.decode())
+  {
+    translateIR();
+    irrecv.resume();
+  }
+}
+
+//hello
